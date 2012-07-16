63fa74a2f957c38df3d642e258c858fefe95dfbc
Christopher Popp <chr1star@gmail.com>
2012-07-15 19:56:29 -0500

    first ruby todo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'd5ec835839aee8e03f40f5b3d24147ac161f38fc8a05d70c83d11787f2298054a7cfa56734d4dbbec03afa606420d97d1a91942e8b68f480b246ca607186fe62'
